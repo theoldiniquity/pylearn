75a923b839def08ccc5fada625e302b690ded8fe
Maxim Safronov <safmax@gmail.com>
2023-03-31 23:53:50 +0400

    Add add function

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def add(a, b):
+    return a + b
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
